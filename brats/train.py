3b33cd83edb948a3028cc8964db4332a1a18a392
Mingrui Jiang <mjray369@gmail.com>
2018-02-02 15:23:07 +0800

    tweak settings

--- brats/train.py
+++ brats/train.py
@@ -10,7 +10,7 @@ from unet3d.training import load_old_model, train_model
 config = dict()
 config["pool_size"] = (2, 2, 2)  # pool size for the max pooling operations
 config["image_shape"] = (256, 256, 24)#(256, 256, 24)  # This determines what shape the images will be cropped/resampled to.
-config["patch_shape"] = (128, 128, 24)  # switch to None to train on the whole image
+config["patch_shape"] = (64, 64, 24)  # switch to None to train on the whole image
 config["labels"] = (1, )  # the label numbers on the input image
 config["n_labels"] = len(config["labels"])
 config["all_modalities"] = ["t1", "t1Gd", "flair", "t2"]
@@ -24,8 +24,8 @@ else:
 config["truth_channel"] = config["nb_channels"]
 config["deconvolution"] = True  # if False, will use upsampling instead of deconvolution
 
-config["batch_size"] = 4
-config["validation_batch_size"] = 6
+config["batch_size"] = 8
+config["validation_batch_size"] = 12
 config["n_epochs"] = 500  # cutoff the training after this many epochs
 config["patience"] = 10  # learning rate will be reduced after this many epochs if the validation loss is not improving
 config["early_stop"] = 40  # training will be stopped after this many epochs without the validation loss improving
@@ -34,7 +34,7 @@ config["learning_rate_drop"] = 0.5  # factor by which the learning rate will be 
 config["validation_split"] = 0.8  # portion of the data that will be used for training
 config["flip"] = True  # augments the data by randomly flipping an axis during
 config["permute"] = False  # data shape must be a cube. Augments the data by permuting in various directions
-config["distort"] =  None # switch to None if you want no distortion
+config["distort"] =  0.5 # switch to None if you want no distortion
 config["augment"] = config["flip"] or config["distort"]
 config["validation_patch_overlap"] = 0  # if > 0, during training, validation patches will be overlapping
 config["training_patch_start_offset"] = (16, 16, 16)  # randomly offset the first patch index by up to this offset
