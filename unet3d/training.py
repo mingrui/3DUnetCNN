7c2d9bb5d49dcd86382f9eea2e407c83c990b04e
Mingrui Jiang <mjray369@gmail.com>
2018-02-02 23:38:26 +0800

    tweaks

--- unet3d/training.py
+++ unet3d/training.py
@@ -11,7 +11,7 @@ from unet3d.metrics import (dice_coefficient, dice_coefficient_loss, dice_coef, 
 import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
 config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.9
+config.gpu_options.per_process_gpu_memory_fraction = 0.8
 set_session(tf.Session(config=config))
 
 K.set_image_dim_ordering('th')
